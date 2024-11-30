f49b8ccd31b3f43f367f8866448a037793ffb811
tacheraSasi <tacherasasi@gmail.com>
2024-12-01 02:40:59 +0300

    sending simple post req to /chat/

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .chatbot import get_answer
 
 def home(request):
     return render(request,"index.html")
 
 
 def chat(request):
-    return
+    if request.method == "POST":
+        input = request.POST["input"]
+        return print(get_answer(input))
+    return "Invalid method"
